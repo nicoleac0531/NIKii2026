df8973e3c8cd218ab2e05528e905c6b7f6df2741
Angie-lab-2308 <angie.quesadaobando@ucr.ac.cr>
2026-05-03 10:49:00 -0600

    Update ProyectoIntegradorG01.py

--- ProyectoIntegradorG01.py
+++ ProyectoIntegradorG01.py
@@ -1,1 +1,17 @@
 nombre_usuario: str = ''
+#Input
+mensaje:str = ""
+operario_revision = str ( input ("Digite el nombre del operario a cargo de la revisión "))
+botellas_producidas = int ( input ("Digite el número de botellas producidas en el turno "))
+botellas_defectuosas = int ( input ("Digite el número de botellas defectuosas "))
+porcentaje_defectos = str
+
+#Process
+porcentaje_defectos=(botellas_defectuosas/botellas_producidas)*100
+if porcentaje_defectos <= 5.0:
+    mensaje = "Meta cumplida"
+elif porcentaje_defectos > 5.0:
+    mensaje = "Meta no cumplida"
+
+#Output
+print ("El resultado es " + mensaje )
