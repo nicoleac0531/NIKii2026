95234ed5a28353d5cf4b967bc96849374c9a6c1f
nicoleac0531 <nicole.alvarezcuevas@ucr.ac.cr>
2026-05-03 10:55:17 -0600

    Update ProyectoIntegradorG01.py

--- ProyectoIntegradorG01.py
+++ ProyectoIntegradorG01.py
@@ -1,17 +1,20 @@
-nombre_usuario: str = ''
+operario_revision: str = ''
+botellas_producidas: int = 0
+botellas_defectuosas: int = 0
+porcentaje_defectos = float = 0.0
+mensaje:str = ''
+
 #Input
-mensaje:str = ""
 operario_revision = str ( input ("Digite el nombre del operario a cargo de la revisión "))
 botellas_producidas = int ( input ("Digite el número de botellas producidas en el turno "))
 botellas_defectuosas = int ( input ("Digite el número de botellas defectuosas "))
-porcentaje_defectos = str
 
 #Process
 porcentaje_defectos=(botellas_defectuosas/botellas_producidas)*100
 if porcentaje_defectos <= 5.0:
     mensaje = "Meta cumplida"
-elif porcentaje_defectos > 5.0:
+else porcentaje_defectos > 5.0:
     mensaje = "Meta no cumplida"
 
 #Output
-print ("El resultado es " + mensaje )
+print ("El resultado es: " + mensaje )
